407fca6b568dbb3975b23c4970a4c0bbd1898a61
Mahesh Gondi <mashf13@gmail.com>
2016-08-22 06:16:40 +0200

    Docker setup

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM ubuntu:16.04
+MAINTAINER mashf13
+
+ADD ./ /app
+
+RUN apt-get -y update
+RUN apt-get install -y wget unzip default-jdk redis-tools
+RUN wget http://www.scala-lang.org/files/archive/scala-2.11.6.deb -o nul
+RUN dpkg -i scala-2.11.6.deb
+RUN apt-get -y update
+RUN apt-get -y install scala
+WORKDIR /opt
+RUN wget http://downloads.typesafe.com/typesafe-activator/1.3.2/typesafe-activator-1.3.2-minimal.zip
+RUN unzip typesafe-activator-1.3.2-minimal.zip
+RUN mv activator-1.3.2-minimal activator
+RUN ln -s /opt/activator/activator /usr/local/sbin/activator
+
+WORKDIR /app
+RUN activator compile
+ENTRYPOINT ["/app/script.sh"]
+
---
+++ app/controllers/TwitterController.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import akka.actor.ActorSystem;
+import play.api.Configuration;
+import play.mvc.*;
+
+import scala.concurrent.ExecutionContextExecutor;
+import services.TwitterService;
+
+import javax.inject.Inject;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import views.html.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class TwitterController extends Controller {
+
+    private final Configuration conf;
+    private TwitterService twitterService;
+    private ActorSystem actorSystem;
+    private ExecutionContextExecutor exec;
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+
+    @Inject
+    public TwitterController(TwitterService twitterService, ActorSystem actorSystem, ExecutionContextExecutor exec, Configuration conf) {
+        this.twitterService = twitterService;
+        this.actorSystem = actorSystem;
+        this.exec = exec;
+        this.conf = conf;
+    }
+    public CompletableFuture<Result> getWordCount() {
+        CompletableFuture<Result> future = CompletableFuture
+                .supplyAsync(() -> twitterService.getTopNWords(50))
+                .thenApply(lst -> ok(wc.render(lst, "Twitter Word Count")));
+
+        return future;
+    }
+    public CompletableFuture<Result> getEventsCount() {
+        CompletableFuture<Result> future = CompletableFuture
+                .supplyAsync(() -> twitterService.getEvents())
+                .thenApply(lst -> ok(wc.render(lst, "Twitter Events")));
+
+        return future;
+    }
+}
